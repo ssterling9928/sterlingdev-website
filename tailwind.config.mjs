59ad2eeb4781cf253c8448b7045edc9a2b894858
Stephen <ssterling9928@yahoo.com>
2025-06-23 21:15:11 -0500

    - More Styling Updates - Header and Hero Section - Repsonsive Editing - Created new Nav Link Pages - Set up links in Nav bar to work. - Added new custom font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,19 @@
 
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}' // <-- Crucial line
+
   ],
   theme: {
     extend: {
-      // Your custom Tailwind theme extensions (colors, fonts, etc.)
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+
+        heading: ["Fira Sans Condensed", ...defaultTheme.fontFamily.sans], 
+      }
     }
   },
   plugins: [],
